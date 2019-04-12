caef4a75b730769def4b90a38c49bc2bf9135d4b
俞乐诚 <yulecheng@gmail.com>
2019-04-12 14:55:04 +0800

    Site updated: 2019-04-12 14:55:04

--- js/main.js
+++ js/main.js
@@ -241,11 +241,8 @@
         },
         reward: function () {
             var modal = new this.modal('#reward');
-            // $('#rewardBtn').addEventListener(even, modal.toggle);
-            $('#rewardBtn').addEventListener(even, function(){
-                mask.parentNode.appendChild($('#reward'));
-                modal.toggle();
-            });
+            $('#rewardBtn').addEventListener(even, modal.toggle);
+
             var $rewardToggle = $('#rewardToggle');
             var $rewardCode = $('#rewardCode');
             if ($rewardToggle) {
